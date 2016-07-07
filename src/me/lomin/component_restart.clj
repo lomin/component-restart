98c0e12d428259c52deba4ae9661780ac156ad09
lomin <happylomin@gmail.com>
2016-07-07 14:45:44 +0200

    Fix for Issue #1 (https://github.com/lomin/component-restart/issues/1): component-restart handles dashes in namespaces just fine now.

--- src/me/lomin/component_restart.clj
+++ src/me/lomin/component_restart.clj
@@ -1,5 +1,4 @@
 (ns me.lomin.component-restart
-  (:import (java.io File))
   (:require [clojure.string :as str]
             [clojure.tools.logging :as log]
             [clojure.tools.namespace.dir :as dir]
@@ -14,25 +13,29 @@
   (str "/" (to-relative-resource a-meta)))
 
 (defn ns-symbol [a-meta]
-  (symbol (str/replace (to-relative-resource a-meta) #"/" ".")))
+  (-> (to-relative-resource a-meta)
+      (str/split  #"/")
+      (as-> parts (conj (pop parts) (str/replace (last parts) #"_" "-")))
+      (->> (str/join "."))
+      (symbol)))
 
 (defn load-clojure-file [file-name]
   (log/debug (str "trying to load <" file-name ">"))
   (load file-name)
   (log/debug "file successfully loaded."))
 
-(defn restart-callback [ns-sym f-sym]
-  (log/debug (str  "starting callback <" ns-sym "><" f-sym ">"))
-  ((ns-resolve ns-sym f-sym))) 
+(defn resolve-callback [a-meta]
+  (let [ns-sym (ns-symbol a-meta)
+        f-sym (:name a-meta)]
+    (log/debug (str "starting callback <" ns-sym "><" f-sym ">"))
+    (ns-resolve ns-sym f-sym)))
 
 (defn reload [a-var]
   (let [a-meta (meta a-var)
         file-name (ns-file-name a-meta)]
     (repl/refresh)
     (load-clojure-file file-name)
-    (restart-callback
-      (ns-symbol a-meta)
-      (symbol (:name a-meta)))))
+    ((resolve-callback a-meta))))
 
 (defn watch
   ([callback-var system]
--- test/me/lomin/component_restart_test.clj
+++ test/me/lomin/component_restart_test.clj
@@ -1,3 +1,15 @@
 (ns me.lomin.component-restart-test
   (:require [clojure.test :refer :all]
-            [me.lomin.component-restart :refer :all]))
+            [me.lomin.component-restart :as cr]))
+
+(defn constant-three []
+  3)
+
+(deftest resolve-callback-test
+  (let [a-meta (meta (var constant-three))]
+    (= "/me/lomin/component_restart_test"
+       (cr/ns-file-name a-meta))
+    (is (= (symbol "me.lomin.component-restart-test")
+           (cr/ns-symbol a-meta)))
+    (is (= (var constant-three)
+           (cr/resolve-callback (meta (var constant-three)))))))
