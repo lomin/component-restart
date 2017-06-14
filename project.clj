1093da2c6b783502867d47fb056726c3aec381ff
Kai Brandes <Kai Brandes>
2017-06-14 10:51:15 +0200

    changed project basename

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject me.lomin/component-restart "0.2.0"
+(defproject kaibra/component-restart "0.2.0"
             :description "A simple watcher that restarts components when files change."
-            :url "https://github.com/lomin/component-restart"
+            :url "https://github.com/kaibra/component-restart"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.6.0"]
